c141a8909801c0f19679880d9389db1fc43467cd
kyo144 <kwsmcy@gmail.com>
2023-04-15 21:08:19 +0800

    feat: build icon

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -2,13 +2,49 @@ import { ComponentProps, forwardRef } from 'react';
 import { styled } from '@styles/stitches.config';
 
 const Symbol = styled('i', {
-  fontVariationSettings: `'FILL' 0, 'wght' 500, 'GRAD' 0, 'opsz' 48`,
+  display: 'block',
+  textAlign: 'center',
   variants: {
+    bold: {
+      true: {},
+    },
     fill: {
-      true: {
+      true: {},
+    },
+  },
+  compoundVariants: [
+    {
+      bold: true,
+      fill: true,
+      css: {
+        fontVariationSettings: `'FILL' 1, 'wght' 700, 'GRAD' 0, 'opsz' 48`,
+      },
+    },
+    {
+      bold: true,
+      fill: false,
+      css: {
+        fontVariationSettings: `'FILL' 0, 'wght' 700, 'GRAD' 0, 'opsz' 48`,
+      },
+    },
+    {
+      bold: false,
+      fill: true,
+      css: {
         fontVariationSettings: `'FILL' 1, 'wght' 500, 'GRAD' 0, 'opsz' 48`,
       },
     },
+    {
+      bold: false,
+      fill: false,
+      css: {
+        fontVariationSettings: `'FILL' 0, 'wght' 500, 'GRAD' 0, 'opsz' 48`,
+      },
+    },
+  ],
+  defaultVariants: {
+    fill: false,
+    bold: false,
   },
 });
 
