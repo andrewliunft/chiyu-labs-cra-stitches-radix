221811be6280930b1fae2304c286edd76ef78ba7
kyo144 <kwsmcy@gmail.com>
2023-04-17 00:40:58 +0800

    feat: modify box shadow md

--- src/styles/stitches.config.ts
+++ src/styles/stitches.config.ts
@@ -89,7 +89,7 @@ export const { styled, css, globalCss, keyframes, getCssText, theme, createTheme
     },
     shadows: {
       sm: '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)',
-      md: '0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)',
+      md: '0px 0px 6px rgba(0, 0, 0, 0.2);',
     },
     zIndices: {
       z0: 0,
